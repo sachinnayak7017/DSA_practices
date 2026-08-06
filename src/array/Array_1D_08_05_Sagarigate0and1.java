cc5c6b9768418bd2e48ca90dcb688eb945d5cdb3
sachinnayak7017 <sachinnayak7017@gmail.com>
2026-08-07 00:44:46 +0530

    06-08-2026-07-08-2026

--- src/array/Array_1D_08_05_Sagarigate0and1.java
+++ src/array/Array_1D_08_05_Sagarigate0and1.java
@@ -7,17 +7,20 @@ public class Array_1D_08_05_Sagarigate0and1 {
     public static void main(String[] args) {
 
         int[] arr = {1,0,1,0,1,0,0,0};
+        int start = 0;
+        int end = arr.length-1;
+        int n = arr.length;
+        while(start < end){
 
-         int zero = 0;
-        int one =0;
-        for(int ar : arr){
-            if(ar==0) zero++;
-            else one ++;
-        }
-        for(int i =0; i < arr.length; i++){
-            if(i<zero) arr[i]=0;
-            else arr[i]=1;
-        }
+            if(arr[start]==0) start ++;
+            if(arr[end]==1) end --;
+            if(start>end) break;;
+            if(arr[start]==1 && arr[end]==0){
 
+            arr[start ]=0;
+            arr[end] = 1;
+            start++;
+            end--;
+        }
     }
-}
+}}
---
+++ src/array/Array_1D_08_06_WaveArray.java
@@ -0,0 +1,13 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Array_1D_08_06_WaveArray {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> arr = new  ArrayList<>();
+
+    }
+}
